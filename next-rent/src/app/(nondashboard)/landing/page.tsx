53e4687d45d8caff87e7fe1b84be8440cc009157
June Suparoek <juneidea@yahoo.com>
2025-06-21 23:07:00 -0500

    landing

--- next-rent/src/app/(nondashboard)/landing/page.tsx
+++ next-rent/src/app/(nondashboard)/landing/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import HeroSection from "./HeroSection";
+import FeaturesSection from "./FeaturesSection";
 
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <div>
+      <HeroSection />
+      <FeaturesSection />
+    </div>
+  );
 };
 
 export default Landing;
--- next-rent/src/app/(nondashboard)/layout.tsx
+++ next-rent/src/app/(nondashboard)/layout.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { NAVBAR_HEIGHT } from "@/lib/constants";
+import Navbar from "@/components/Navbar";
 
-const Layout = () => {
-  return <div>Layout</div>;
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full w-full">
+      <Navbar />
+      <main
+        className={`h-full w-full flex flex-col`}
+        style={{ paddingTop: `${NAVBAR_HEIGHT}px` }}
+      >
+        {children}
+      </main>
+    </div>
+  );
 };
 
 export default Layout;
